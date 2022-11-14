7e7dc09f0ecba44543f4132c7cafd11346073711
Oyama Yorushi <oyama.rosa@hotmail.com>
2022-11-13 21:40:44 -0300

    Adicionado README e refatorado o codigo.

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1 align="center">Timer</h1>
+<p>Este é um projeto simples em JavaScript usando as técnicas de interação com o D.O.M., manipulação de eventos e formatação de datas com toLocaleTimeString. </p>
--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function timer(){
     const buttonRestart = document.querySelector('#restart');
     let timer = document.querySelector('#setTimer')
     let appTimer;
-    let seconds = 0;
     
     function addClass(element){
         element.classList.add('paused')
